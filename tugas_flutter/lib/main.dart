ced6b56e6d4e779691bc9cdf40e63c1bacf22d42
agustina12 <gustina200@gmail.com>
2021-12-10 15:09:04 +0800

    Tina_Tugas_Selesai

--- tugas_flutter/lib/main.dart
+++ tugas_flutter/lib/main.dart
@@ -41,40 +41,155 @@ class TugasKedua extends StatelessWidget {
         child: Container(
           child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(5, 10, 5, 8),
-                child: Center(
-                  child: Text(
-                    "Daftar Barang Masuk",
-                    style: TextStyle(
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child:Text(
+                      "Daftar Tontonan Anime",
+                      style: TextStyle(
                       fontSize: 18,
-                      fontWeight: FontWeight.w900, 
-                    ),
-                  ),
-                ),
+                      fontWeight: FontWeight.w900,
+                      color: Colors.black),
+                    ), 
+                  ), 
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(3.0),
+                    child: Image.network("https://lh3.googleusercontent.com/S8-JimYp0uyLZAXoRinTe9O_FqGjDn3COHWuhYlvkZvS8EvBqdxKiEZa7inp2_IGmnk", 
+                    height: 20,),
+                  ) 
+                  
+                  // Icon(Icons.movie, color: Colors.red,),
+                ],
               ),
               Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
+                padding: const EdgeInsets.fromLTRB(5, 8, 5, 10),
                 child: Text(
-                  "PT. Makmur Sentosa",
+                  "Aplikasi Nonton Anime",
                   style: TextStyle(
                     fontSize: 14,
                     fontStyle: FontStyle.italic),
                 ),
               ),
               Container(
-                height: 600,
+                height: 720,
                 child: ListView(
                   children: [
+                    // Padding(padding: const EdgeInsets.only(left: 5.0),
                     ListTile(
-                      leading: Text('1'),
-                      title: Text('Meja merek A'),
-                      subtitle: Text('20 buah'),
+                      // horizontalTitleGap: 0,
+                      // jarak line dari kiri kekanan 
+                      leading: Text('1', 
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,),
+                      ),
+                      title: Text('Kaizoku Oujo'),
                       trailing: Icon(
-                        Icons.favorite,
-                        color: Colors.red,
+                        Icons.delete_outline
                         ),
                     ),
+            
+                    Divider(height: 50), //garis
+                    ListTile(
+                      leading: Text('2', 
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red),
+                      ),
+                      title: Text('Re-Main'),
+                      trailing: Icon(
+                        Icons.delete_outline
+                      ),
+                    ),
+                    Divider(height: 50), //garis
+                    ListTile(
+                      leading: Text('3', 
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red),
+                      ),
+                      title: Text('Shiroi Suna no Aquatope'),
+                      trailing: Icon(
+                        Icons.delete_outline
+                      ),
+                    ),
+                    Divider(height: 50),
+                    ListTile(
+                      leading: Text('4', 
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red),
+                      ),
+                      title: Text('Yakunara Mug Cup mo: Niban Gama'),
+                      trailing: Icon(
+                        Icons.delete_outline
+                      ),
+                    ),
+                    Divider(height: 50), //garis     
+                    ListTile(
+                      leading: Wrap(
+                        children: [
+                          Icon(Icons.check_box_outlined),
+                        ],
+                        
+                      ),
+                      title: Text('Demon Slayer', 
+                            style: TextStyle(
+                              fontSize: 16, 
+                              fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        'Saya adalah karakter dianime demon slayer sebagai tanjiro kakanya netsuko',
+                        style: TextStyle(
+                          fontSize: 12, 
+                          fontWeight: FontWeight.w400,
+                          height: 1.2,
+                          letterSpacing: 0.2),
+                      ),
+                      trailing: Wrap(
+                        spacing: 10,
+                        children: [
+                          Icon(Icons.remove_red_eye_outlined),
+                          Icon(Icons.favorite_outline),
+                        ],
+                      ),
+                    ),
+                    Divider(height: 50), //garis     
+                    ListTile(
+                      leading: Wrap(
+                        children: [
+                          Icon(Icons.check_box_outlined),
+                        ],
+                        
+                      ),
+                      title: Text('Nama saya Tanjiro', 
+                            style: TextStyle(
+                              fontSize: 16, 
+                              fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        'Saya adalah karakter dianime demon slayer sebagai tanjiro kakanya netsuko',
+                        style: TextStyle(
+                          fontSize: 12, 
+                          fontWeight: FontWeight.w400,
+                          height: 1.2,
+                          letterSpacing: 0.2),
+                      ),
+                      trailing: Wrap(
+                        spacing: 10,
+                        children: [
+                          Icon(Icons.remove_red_eye_outlined),
+                          Icon(Icons.favorite_outline),
+                        ],
+                      ),
+                    ),
+
                   ],
                 ),
               ),
